e2442d2ed6f1c1f2cafa3c06aed6ab63a8fe947d
Coleman Rose <colemanrose3@yahoo.com>
2020-11-22 17:18:09 +0100

    Dashboard component is the go-to.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import { URLBar } from './Components/URLBar/URLBar';
 import { Dashboard } from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <URLBar />
+        <Dashboard />
       </header>
     </div>
   );
