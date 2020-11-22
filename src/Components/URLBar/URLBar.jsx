6aea1a2a398c39f0267add76436f15179a7849b5
Coleman Rose <colemanrose3@yahoo.com>
2020-11-22 14:57:38 +0100

    Starting to add an event handler on the URLBar to track what is in the text field.

--- src/Components/URLBar/URLBar.jsx
+++ src/Components/URLBar/URLBar.jsx
@@ -1,11 +1,23 @@
 import { Component } from 'react'
 import styles from './styles.module.scss'
+import axios from 'axios'
 
 export class URLBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { url: '' }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ url: event.target.value })
+    }
+
     render() {
         return (
             <div className={styles.form}>
-                <input type="text" name="Github URL" autoComplete="off" />
+                <input onChange={this.handleChange} value={this.state.url} type="text" name="Github URL" autoComplete="off" />
                 <label for="name" className={styles.labelName}>
                     <span className={styles.contentName}>Github URL</span>
                 </label>
