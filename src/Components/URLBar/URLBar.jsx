434ceefb824a8b3bf57dcc0025d3171f0fa8bd2c
Coleman Rose <colemanrose.new@gmail.com>
2020-12-13 10:56:13 +0100

    Bug fix in the css pog :))))))))))

--- src/Components/URLBar/URLBar.jsx
+++ src/Components/URLBar/URLBar.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 export class URLBar extends Component {
     constructor(props) {
         super(props)
-        this.state = { url: '' }
+        this.state = { url: '', user: null }
         
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -26,18 +26,18 @@ export class URLBar extends Component {
             throw err;
         })
         console.log(request)
-        return request
-        event.preventDefault();
+        this.setState({ user: JSON.stringify(request.data) })
     }
 
     render() {
         return (
             <div className={styles.form}>
-                <input onChange={this.handleChange} value={this.state.url} type="text" name="Github URL" autoComplete="off" />
+                <input onChange={this.handleChange} placeholder=" " value={this.state.url} type="text" name="Github URL" autoComplete="off" />
                 <label htmlFor="name" className={styles.labelName}>
                     <span className={styles.contentName}>Github URL</span>
                 </label>
                 <button className={styles.submitButton} onClick={this.handleSubmit}>Submit</button>
+                <p>{this.state.user}</p>
             </div>
         )
     } 
